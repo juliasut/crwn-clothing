6404f915609e3c2220a387c6704ba941e80a8b3d
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-18 13:16:49 -0600

    use withRouter to access history prop

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,14 @@
 import './menu-item.styles.scss';
-
-export default function MenuItem({ title, imageUrl, size }) {
+import { withRouter } from 'react-router';
+const MenuItem = ({ title, imageUrl, size, history }) => {
   return (
-    <div style={{
-      backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push()}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
@@ -12,3 +16,5 @@ export default function MenuItem({ title, imageUrl, size }) {
     </div>
   );
 }
+
+export default withRouter(MenuItem);
