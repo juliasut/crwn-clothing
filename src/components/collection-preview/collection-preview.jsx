262f3624927d42bc2f7ed89388d2cffd5f56bbfe
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-18 16:13:05 -0600

    filter only four items as a collection preview

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -5,7 +5,10 @@ export default function CollectionPreview({ title, items }) {
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map((item) => (
+        {items
+        // will show only four items as a preview. might affect the performance if the data gets very large
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
           <div key={item.id}>{item.name}</div>
         ))}
       </div>
