d798cb7966a832a148a4962d909b338857771e16
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-18 16:08:17 -0600

    Add switch for routing, path for shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
+
 
 function App() {
   return (
-    <div className="App">
-     <HomePage />
+    <div>
+     <Switch>
+       <Route exact path='/' component={HomePage}/>
+       <Route path='/shop' component={ShopPage} />
+     </Switch>
     </div>
   );
 }
