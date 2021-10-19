798d93876009556480a7eaf23d86a1ba3a508549
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-18 23:55:53 -0600

    create reusable input component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,19 @@
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {label ? (
+      // add "shrink" property whenever a user has typed smth in
+      <label
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
