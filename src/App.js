d091c02044919b39387c8c2b84fa83392eb837d9
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-19 22:38:43 -0600

    store user document in firestore db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,12 @@ class App extends React.Component {
 
   componentDidMount() {
     // we've subscribed for the application to listen to the auth, the auth is sending the user object every time untill they sign out
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        await createUserProfileDocument(userAuth);
+      }
+    }
+    );
   }
 
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,31 +1,58 @@
 // use /compat/ to make version 8 firebase imports work in version 9
 import firebase from 'firebase/compat/app';
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider } from 'firebase/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const config = {
-  apiKey: "AIzaSyClztTswirn-nlN0YJHXAZNb5-aLbO8qB0",
-  authDomain: "crwn-db-b2fad.firebaseapp.com",
-  projectId: "crwn-db-b2fad",
-  storageBucket: "crwn-db-b2fad.appspot.com",
-  messagingSenderId: "291549530024",
-  appId: "1:291549530024:web:045cc44fe9f69907d2ff9d"
+  apiKey: 'AIzaSyClztTswirn-nlN0YJHXAZNb5-aLbO8qB0',
+  authDomain: 'crwn-db-b2fad.firebaseapp.com',
+  projectId: 'crwn-db-b2fad',
+  storageBucket: 'crwn-db-b2fad.appspot.com',
+  messagingSenderId: '291549530024',
+  appId: '1:291549530024:web:045cc44fe9f69907d2ff9d',
 };
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // snapShot represents the data of the user object. we'll use only some properties to save a user to our users collection
+  const snapShot = await userRef.get();
+
+  // if the property exists on the snapShot is false, we'll create a new user
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+
 export const auth = firebase.auth();
-export const firestore =  firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new GoogleAuthProvider();
 // trigger Google account pop-up when we use this auth provider
 provider.setCustomParameters({ prompt: 'select_account' });
 
-
 // choosing Google popup out of multiple possible ones by passing the provider we've assigned
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
-}
+};
 
 export default firebase;
