b1eee3ea32d1021646a1d9917178eb6a04661a9b
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-20 11:49:22 -0600

    feed App the currentUser prop from reducer instead of state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
   // setting a property on our class
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     // we've subscribed for the application to listen to the auth, the auth is sending the user object every time untill they sign out
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -26,18 +23,14 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            }
-          );
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -60,4 +53,10 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  // dispatch passes the object user as action object to every reducer. We're invoking setCurrentUser with the user that will then be used as a payload
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+// App doesn't need currentUser any more, so we put null as the first argument instead of mapStateToProps function
+export default connect(null, mapDispatchToProps)(App);
