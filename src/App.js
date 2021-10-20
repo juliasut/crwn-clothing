1e41268de65cf45094755e232c443ffee0567b46
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-20 13:53:35 -0600

    update app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
@@ -46,12 +46,16 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = (dispatch) => ({
   // dispatch passes the object user as action object to every reducer. We're invoking setCurrentUser with the user that will then be used as a payload
@@ -59,4 +63,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 // App doesn't need currentUser any more, so we put null as the first argument instead of mapStateToProps function
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
