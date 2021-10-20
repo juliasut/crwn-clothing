d964fc1616a19d0611ccd54007921eeba77c7c39
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-20 15:03:19 -0600

    add cartDropdown component and cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,14 @@
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
-import {auth } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 // Higher order component HOC
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 // now gets fed currentUser prop from redux
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -18,20 +20,25 @@ const Header = ({currentUser}) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {
-        currentUser
-        ?
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-        <Link className="option" to="/signin">SIGN IN</Link>
-
-      }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
 // allows to access the state - root reducer, then access user reducer, from there - the currentUser value of state
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+// destructure state
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 export default connect(mapStateToProps)(Header);
