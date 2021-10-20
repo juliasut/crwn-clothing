098aaeb7f0c0e20c7c7e1de8763d92a69e38a6ac
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-19 20:07:42 -0600

    rewrite App as class component to get access to state, set auth subscription to firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-     <Switch>
-       <Route exact path='/' component={HomePage}/>
-       <Route path='/shop' component={ShopPage} />
-       <Route path='/signin' component={SignInAndSignUpPage} />
-     </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null,
+    };
+  }
+  // setting a property on our class
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // we've subscribed for the application to listen to the auth, the auth is sending the user object every time untill they sign out
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // close the subscription to prevent memory leaks
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
