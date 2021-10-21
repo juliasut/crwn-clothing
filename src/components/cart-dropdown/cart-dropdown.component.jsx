e207820559183b9d637f90a9a23ed5307fc4772e
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-21 13:00:42 -0600

    add comment explaining the choice for memoizable selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -15,6 +15,7 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
+// redux's mapStateToProps has a shallow equality check for every value in the object; it won't replace values if they pass a shallow equality check which means it won't needlessly re-render, but if we have transformation logic it's still valuable to memoize it with a selector to save us running duplicate logic to get the same output.
 const mapStateToProps = (state) => ({
   cartItems: selectCartItems(state),
 });
