c132e07b89a7930147974f8190bdc5e5bbff0ce4
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-20 21:32:55 -0600

    add reducer to manage cart items state

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -9,8 +9,8 @@ export default function CollectionPreview({ title, items }) {
         {items
           // will show only four items as a preview. might affect the performance if the data gets very large
           .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemprops }) => (
-            <CollectionItem key={id} {...otherItemprops} />
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,5 +2,11 @@ import { CartActionTypes } from './cart.types';
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
+
   // not using payload property in state
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
 });
