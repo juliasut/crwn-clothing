3d36fd31107713422958b2d15c2db12e777094d9
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-25 23:57:43 -0600

    add checkout header

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,26 @@
 import './checkout.styles.scss';
 
-const CheckoutPage = () => <div>Checkout Page</div>;
+const CheckoutPage = () => (
+
+  <div className='checkout-page'>
+    <div className='checkout-header'>
+      <div className='header-block'>
+        <span>Product</span>
+      </div>
+      <div className='header-block'>
+        <span>Description</span>
+      </div>
+    </div>
+      <div className='header-block'>
+        <span>Quantity</span>
+      </div>
+      <div className='header-block'>
+        <span>Price</span>
+      </div>
+      <div className='header-block'>
+        <span>Remove</span>
+      </div>
+  </div>
+)
 
 export default CheckoutPage;
