cf59e184c7bf9ffb9091e055f92d95452ea6e855
juliasut <juliasutygina@gmail.com>
2022-01-04 23:39:14 -0700

    Move collections overview into its own component

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../collection-preview/collection-preview.jsx';
-
-import { selectCollections } from '../../redux/shop/shop.selectors.js';
+import CollectionPreview from '../collection-preview/collection-preview.component.jsx';
+import CollectionsOverview from '../collections-overview/collections-overview.component.jsx';
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
