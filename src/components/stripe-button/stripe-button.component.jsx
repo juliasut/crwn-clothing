ff562d3b84aad0c94c6845faad55077dad9b3dda
juliasut <juliasutygina@gmail.com>
2022-01-06 00:37:16 -0700

    Integrate stripe payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JQiTsC1Kd0vNM1lBHTNPz29pxPSZIXbQ0rnYvkJjvKdYSTT5eTUWRY5CBEJ9hZafmUyuBfq6HcYqqYeLteToOmE00o852zpfv';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Closing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
