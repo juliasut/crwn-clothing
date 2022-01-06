31a33ed46bd8c760e1eb9e960efc39f79cbefadb
juliasut <juliasutygina@gmail.com>
2022-01-05 23:52:23 -0700

    Update ux flow flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,11 +6,19 @@ import './collection.styles.scss';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
-const CollectionPage = () => (
-  <div className="Collection-page">
-    <h2>Collection Page</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
