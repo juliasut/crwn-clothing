6d17bc170452e0874ab055c34033e84595ddfde8
juliasut <juliasutygina@gmail.com>
2022-01-20 22:29:09 -0700

    Refactor with styled-components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,10 @@
 import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 export default function HomePage() {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 }
